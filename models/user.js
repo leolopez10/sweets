0d741c7b533dca6137ade0774e3e04b74cd8415e
lil L <57925562+leolopez10@users.noreply.github.com>
2020-03-18 17:55:08 -0500

    created a userSchema

---
+++ models/user.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Scehma = mongoose.Schema;
+const crypto = require('crypto');
+const uuidv1 = require('uuid/v1');
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        require: true,
+        maxlength: 32
+    },
+    email: {
+        type: String,
+        trim: true,
+        require: true,
+        unique: 32
+    },
+    hashed_password: {
+        type: String,
+        trim: true,
+    },
+    about: {
+        type: String,
+        trim: true,
+    },
+    salt: {
+        type: String,
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    history: {
+        type: Array,
+        default: []
+    },
+}, { timestamps: true })
