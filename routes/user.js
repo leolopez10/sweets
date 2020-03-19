11fc82cc3709c460aba4ce7be4102d01914f9542
lil L <57925562+leolopez10@users.noreply.github.com>
2020-03-19 13:38:03 -0500

    seperated  user routes and auth routes

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const router = express.Router();
+//controllers
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
+const { userById } = require("../controllers/user");
 
-const { sayHi } = require("../controllers/user");
+//temp route
+router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
+    console.log(req.profile);
+    res.json({
+        user: req.profile
+    });
+});
 
+//middleware for getting user by id above (user: req.profile)
+router.param("userId", userById);
 
-router.get("/", sayHi)
 
 
 module.exports = router;
