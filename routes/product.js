b33def5a11fc7e70bf99f1a80a5c85410845ca24
lil L <57925562+leolopez10@users.noreply.github.com>
2020-03-23 17:45:07 -0500

    created a route and method to get a client's photo

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 //controllers
-const { create, productById, read, remove, update, list, listRelated, listCategories, listBySearch } = require("../controllers/product");
+const { create, productById, read, remove, update, list, listRelated, listCategories, listBySearch, photo } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
@@ -10,10 +10,11 @@ router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
 router.delete("/product/:productId/:userId", requireSignin, isAuth, isAdmin, remove);
 router.put("/product/:productId/:userId", requireSignin, isAuth, isAdmin, update);
 
+router.get("/product/photo/:productId", photo);
+
 router.get("/products", list);
 router.get("/products/related/:productId", listRelated);
 router.get("/products/categories", listCategories);
-
 router.post("/products/by/search", listBySearch);
 
 router.param("userId", userById);
