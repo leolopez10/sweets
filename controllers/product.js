b97742ec09dadfed54cd7daa85c8f246282621f1
lil L <57925562+leolopez10@users.noreply.github.com>
2020-03-23 17:06:22 -0500

    fixed syntax err with listCategoires method

--- controllers/product.js
+++ controllers/product.js
@@ -170,16 +170,28 @@ exports.list = (req, res) => {
 exports.listRelated = (req, res) => {
     let limit = req.query.limit ? parseInt(req.query.limit) : 6
 
-    Product.find({_id: {$ne: req.product}, category: req.product.category})
+    Product.find({ _id: { $ne: req.product }, category: req.product.category })
         .limit(limit)
         .populate("category", "_id name")
         .exec((err, products) => {
-            if(err) {
+            if (err) {
                 return res.status(400).json({
                     error: "Products not found"
                 });
             }
-            res.send(products)
+            res.json(products)
         });
 };
+
+
+exports.listCategories = (req, res) => {
+    Product.distinct("category", {}, (err, categories) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Categories not found"
+            });
+        }
+        res.json(categories);
+    });
+}
 
