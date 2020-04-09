477683be7f15a20f1e8e893c3b0de0d103c5f92e
lil L <57925562+leolopez10@users.noreply.github.com>
2020-04-09 15:56:18 -0500

    created a simple layout component

--- client/src/user/Signin.js
+++ client/src/user/Signin.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import Layout from '../core/Layout'
 
-const Signin = () => <div>signin</div>
+const Signin = () => (
+    <Layout title="Sign-In Page" description="Sign-In to React E-commerce App">
+        ...
+    </Layout>
+)
 
 export default Signin;
