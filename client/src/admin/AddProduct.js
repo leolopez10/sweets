a5ce379e359dec83c7a6844c8c251c1b58fca157
lil L <57925562+leolopez10@users.noreply.github.com>
2020-04-10 14:55:09 -0500

    created a admin page for creating new products

--- client/src/admin/AddProduct.js
+++ client/src/admin/AddProduct.js
@@ -6,13 +6,159 @@ import { Link } from 'react-router-dom';
 
 const AddProduct = () => {
 
-    const {user, token} = isAuthenticated();
+    
+    const [values, setValues] = useState({
+        name: '',
+        description: '',
+        price: '',
+        categories: [],
+        category: '',
+        shipping: '',
+        quantity: '',
+        photo: '',
+        loading: false,
+        error: '',
+        createdProduct: '',
+        redirectToProfile: false,
+        formData: ''
+    })
+    
+    const {
+        name,
+        description,
+        price,
+        categories,
+        category,
+        shipping,
+        quantity,
+        loading,
+        error,
+        createdProduct,
+        redirectToProfile,
+        formData
+    } = values;
+    
+    const { user, token } = isAuthenticated();
+    
+    useEffect(() => {
+        setValues({ ...values, formData: new FormData() })
+    }, [])
+    
+    //Higher Order function a function returning another function
+    const handleChange = name => event => {
+        const value = name === 'photo' ? event.target.files[0] : event.target.value
+        formData.set(name, value)
+        setValues({ ...values, [name]: value })
+    }
+    
+    const clickSubmit = (event) => {
+        event.preventDefault();
+        setValues({...values, error: '', loading: true});
+        createProduct(user._id, token, formData)
+            .then(data => {
+                if(data.error) {
+                    setValues({...values, error: data.error})
+                } else{
+                    setValues({
+                        ...values,
+                        name: '',
+                        description: '',
+                        photo: '',
+                        price: '',
+                        quantity: '',
+                        loading: false,
+                        createdProduct: data.name
+                    });
+                }
+            });
+    };
+
+    const newPostForm = () => (
+        <form onSubmit={clickSubmit} className="mb-3">
+            <h4>Post Photo</h4>
+            <div className='form-group'>
+                <label className='btn btn-secondary'>
+                    <input
+                        onChange={handleChange('photo')}
+                        type='file'
+                        name='photo'
+                        accept='image/*'
+                    />
+                </label>
+            </div>
+
+            <div className="form-group">
+                <label>Name</label>
+                <input
+                    onChange={handleChange('name')}
+                    type='text'
+                    className='form-control'
+                    value={name}
+                />
+            </div>
+
+            <div className="form-group">
+                <label>Description</label>
+                <textarea
+                    onChange={handleChange('description')}
+                    type='text'
+                    className='form-control'
+                    value={description}
+                />
+            </div>
+
+            <div className="form-group">
+                <label>Price</label>
+                <input
+                    onChange={handleChange('price')}
+                    type='number'
+                    className='form-control'
+                    value={price}
+                />
+            </div>
+
+            <div className="form-group">
+                <label>Category</label>
+                <select
+                    onChange={handleChange('category')}
+                    className='form-control'
+                >
+                    <option value="5e7588c543b37638d4f1b676">Cookie</option>
+                    <option value="5e7588c543b37638d4f1b676">Brownie</option>
+                </select>
+            </div>
+
+            <div className="form-group">
+                <label>Shipping</label>
+                <select
+                    onChange={handleChange('shipping')}
+                    className='form-control'
+                >
+                    <option value="0">No</option>
+                    <option value="1">Yes</option>
+                </select>
+            </div>
+
+            <div className="form-group">
+                <label>Quantity</label>
+                <input
+                    onChange={handleChange('quantity')}
+                    type='number'
+                    className='form-control'
+                    value={quantity}
+                />
+            </div>
+
+            <button className='btn btn-outline-primary'>Create Product</button>
+
+        </form>
+    )
 
     return (
         <Layout title="Add a new Category" description={`G'day ${user.name}, ready to add a new product?`} className="container">
             <div className="row">
                 <div className="col-md-8 offset-md-2">
-                    ...
+                    {newPostForm()}
                 </div>
             </div>
         </Layout>
