43a85b43ec25af999358c2ee55bc6bab8d864eab
lil L <57925562+leolopez10@users.noreply.github.com>
2020-04-13 12:21:40 -0500

    mounting categories to the shop page

--- client/src/core/Shop.js
+++ client/src/core/Shop.js
@@ -1,13 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
-import { getProducts } from './apiCore';
+import { getCategories } from './apiCore';
 import Card from './Card';
 
 const Shop = () => {
+
+    const [categories, setCategories] = useState([]);
+    const [error, setError] = useState(false);
+
+    const init = () => {
+        getCategories()
+            .then(data => {
+                if (data.error) {
+                    setError(data.error)
+                } else {
+                    setCategories(data)
+                }
+            })
+    }
+
+    useEffect(() => {
+        init()
+    }, [])
+
     return (
-        <Layout title="Home Page" description="Node React E-commerce App" className="container-fluid">
+        <Layout title="Shop Page" description="Try out our Baked Goods" className="container-fluid">
             <div className='row'>
-                <div className='col-4'>left sidebar</div>
+                <div className='col-4'>{JSON.stringify(categories)}</div>
                 <div className='col-8'>right</div>
             </div>
         </Layout>
