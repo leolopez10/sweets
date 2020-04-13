70fb42f7e0562cca614a26399193069256415312
lil L <57925562+leolopez10@users.noreply.github.com>
2020-04-13 12:31:06 -0500

    created a checkbox component for people to filter through categories

--- client/src/core/Shop.js
+++ client/src/core/Shop.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
 import { getCategories } from './apiCore';
+import Checkbox from './Checkbox';
 import Card from './Card';
 
 const Shop = () => {
@@ -26,7 +27,13 @@ const Shop = () => {
     return (
         <Layout title="Shop Page" description="Try out our Baked Goods" className="container-fluid">
             <div className='row'>
-                <div className='col-4'>{JSON.stringify(categories)}</div>
+                <div className='col-4'>
+                    {/* {JSON.stringify(categories)} */}
+                    <h4>Filter by Categories</h4>
+                    <ul>
+                        <Checkbox categories={categories} />
+                    </ul>
+                </div>
                 <div className='col-8'>right</div>
             </div>
         </Layout>
