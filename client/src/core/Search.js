843dc74b1bd19a5e21d7efe1b366f8923443a73a
lil L <57925562+leolopez10@users.noreply.github.com>
2020-04-14 11:27:14 -0500

    created a search bar input and drop down for categories

--- client/src/core/Search.js
+++ client/src/core/Search.js
@@ -4,7 +4,7 @@ import { getCategories } from './apiCore';
 import Card from './Card';
 
 const Search = () => {
-    
+
     const [data, setData] = useState({
         categories: [],
         category: '',
@@ -13,15 +13,15 @@ const Search = () => {
         searched: false
     });
 
-    const { categories, category, search, result, searched} = data
+    const { categories, category, search, result, searched } = data
 
     const loadCategories = () => {
         getCategories()
             .then(data => {
-                if(data.error) {
+                if (data.error) {
                     console.log(data.error)
                 } else {
-                    setData({...data, categories: data})
+                    setData({ ...data, categories: data })
                 }
             })
     };
@@ -29,10 +29,49 @@ const Search = () => {
     useEffect(() => {
         loadCategories()
     }, [])
-    
+
+    const handleChange = () => {
+        //
+    };
+
+    const searchSubmit = () => {
+        //
+    };
+
+    const searchForm = () => (
+        <form onSubmit={searchSubmit}>
+            <span className='input-group-text'>
+                <div className='input-group input-group-lg'>
+                    <div className='input-group-prepend'>
+                        <select className='btn mr-2' onChange={handleChange('category')}>
+                            <option value='All'>Pick Category</option>
+                            {categories.map((category, index) => (
+                            <option key={index} value={category._id}>
+                                {category.name}
+                            </option>))}
+                        </select>
+                    </div>
+
+                    <input
+                        type='search'
+                        className='form-control'
+                        onChange={handleChange('search')}
+                        placeholder='Search by name'
+                    />
+                </div>
+
+                <div className='btn input-group-append' style={{border: 'none'}}>
+                    <button className='input-group-text'>Search</button>
+                </div>
+            </span>
+        </form>
+    );
+
     return (
         <div>
-            <h2>Search bar {JSON.stringify(categories)}</h2>
+            <div className='container'>
+                {searchForm()}
+            </div>
         </div>
     );
 };
