d871994b1cf8e74cd763b86e15f28a24c4e769bb
lil L <57925562+leolopez10@users.noreply.github.com>
2020-04-15 13:02:57 -0500

    added functionality to the add to cart button. To save the product within localstorage

--- client/src/core/Card.js
+++ client/src/core/Card.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 import ShowImage from './ShowImage';
 import moment from 'moment'
+import { addItem } from './cartHelpers'
 
 const Card = ({ product, showViewProductButton = true }) => {
+
+    const [redirect, setRedirect] = useState(false)
 
     const showViewButton = (showViewProductButton) => {
         return (
@@ -14,12 +17,24 @@ const Card = ({ product, showViewProductButton = true }) => {
                     </button>
                 </Link >
             )
-        )
-    }
+        );
+    };
+
+    const addToCart = () => {
+        addItem(product, () => {
+            setRedirect(true)
+        });
+    };
+
+    const shouldRedirect = redirect => {
+        if (redirect) {
+            return <Redirect to='/cart' />
+        }
+    };
 
     const showAddToCartButton = () => {
         return (
-            <button className='btn btn-outline-warning mt-2 mb-2'>
+            <button onClick={addToCart} className='btn btn-outline-warning mt-2 mb-2'>
                 Add to Cart
             </button>
         )
@@ -37,13 +52,14 @@ const Card = ({ product, showViewProductButton = true }) => {
         <div className="card">
             <div className='card-header name'>{product.name}</div>
             <div className='card-body'>
+                {shouldRedirect(redirect)}
                 <ShowImage item={product} url='product' />
                 <p className='lead mt-2'>{product.description.substring(0, 100)}</p>
                 <p className='black-10'>${product.price}</p>
                 <p className='black-9'>Category: {product.category && product.category.name}</p>
-                <p class='black-8'>Added {moment(product.createdAt).fromNow()}</p>
+                <p className='black-8'>Added {moment(product.createdAt).fromNow()}</p>
                 {showStock(product.quantity)}
-                <br/>
+                <br />
                 {showViewButton(showViewProductButton)}
                 {showAddToCartButton()}
 
