de8a0a38769c4d3b7e2d04e1ca04f444128dc2c6
lil L <57925562+leolopez10@users.noreply.github.com>
2020-04-17 20:10:30 -0500

    created a backend and front end component that loads all user orders for the admin to see

--- client/src/user/AdminDashboard.js
+++ client/src/user/AdminDashboard.js
@@ -23,6 +23,12 @@ const AdminDashboard = () => {
                         </Link>
                     </li>
 
+                    <li className='list-group-item'>
+                        <Link className="nav-link" to='/admin/orders'>
+                            View Orders
+                        </Link>
+                    </li>
+
                 </ul>
             </div>
         )
