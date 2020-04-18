177421abf6e273d62457ac88281b60b398f5e7eb
lil L <57925562+leolopez10@users.noreply.github.com>
2020-04-17 19:26:14 -0500

    products count decreases and sold increases

--- routes/order.js
+++ routes/order.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { requireSignin, isAuth } = require("../controllers/auth");
+const { decreaseQuantity } = require("../controllers/product");
 const { userById, addOrderToUserHistory } = require("../controllers/user");
 const { create } = require("../controllers/order");
 
-router.post('/order/create/:userId', requireSignin, isAuth, addOrderToUserHistory, create);
+router.post('/order/create/:userId', requireSignin, isAuth, addOrderToUserHistory, decreaseQuantity, create);
 
 
 
