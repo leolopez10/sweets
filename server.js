7e2949186193e43093404e093d1253c2a31cf2e5
lil L <57925562+leolopez10@users.noreply.github.com>
2020-05-18 15:10:42 -0500

    updated server for deployment

--- server.js
+++ server.js
@@ -8,28 +8,20 @@ const cookieParser = require('cookie-parser');
 const expressValidator = require('express-validator');
 
 //import routes
-const authRoutes = require("./routes/auth");
-const userRoutes = require("./routes/user");
-const categoryRoutes = require("./routes/category");
-const productRoutes = require("./routes/product");
-const braintreeRoutes = require("./routes/braintree");
-const orderRoutes = require("./routes/order");
-
-
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+const categoryRoutes = require('./routes/category');
+const productRoutes = require('./routes/product');
+const braintreeRoutes = require('./routes/braintree');
+const orderRoutes = require('./routes/order');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-}
-
 //Check to see if backend is connect to the front end
-app.get("/api/hello", function (req, res) {
-    res.send("SUCCESS ===================Server is talking to the frontend");
-})
-
+app.get('/api/hello', function (req, res) {
+  res.send('SUCCESS ===================Server is talking to the frontend');
+});
 
 //middleware
 app.use(morgan('dev'));
@@ -38,33 +30,37 @@ app.use(cookieParser());
 app.use(expressValidator());
 app.use(cors());
 
-
 //Routes middleware
-app.use("/api", authRoutes);
-app.use("/api", userRoutes);
-app.use("/api", categoryRoutes);
-app.use("/api", productRoutes);
-app.use("/api", braintreeRoutes);
-app.use("/api", orderRoutes);
+app.use('/api', authRoutes);
+app.use('/api', userRoutes);
+app.use('/api', categoryRoutes);
+app.use('/api', productRoutes);
+app.use('/api', braintreeRoutes);
+app.use('/api', orderRoutes);
 
-//Send every other request to the React App
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+//Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  //Send every other request to the React App
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 //Database
-mongoose.connect(process.env.DATABASE, {
+mongoose
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useCreateIndex: true
-})
-.then(() => {
-    console.log("============================Database Connected==============================")
-});
-
-
+  })
+  .then(() => {
+    console.log(
+      '============================Database Connected=============================='
+    );
+  });
 
 //server listening
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
-
