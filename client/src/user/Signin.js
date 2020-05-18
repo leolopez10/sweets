a34c2c5e84fd4eea6f1246f576461024d68587fa
lil L <57925562+leolopez10@users.noreply.github.com>
2020-05-18 15:49:02 -0500

    updated the sign up form to not include an auto feel email, need to update admins thought

--- client/src/user/Signin.js
+++ client/src/user/Signin.js
@@ -4,133 +4,122 @@ import Layout from '../core/Layout';
 import { signin, authenticate, isAuthenticated } from '../auth';
 
 const Signin = () => {
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+    error: '',
+    loading: false,
+    redirectToReferrer: false
+  });
 
-    const [values, setValues] = useState({
-        email: "ben@gmail.com",
-        password: "asdfasdf1",
-        error: "",
-        loading: false,
-        redirectToReferrer: false
+  const { email, password, error, loading, redirectToReferrer } = values;
+  const { user } = isAuthenticated();
+
+  const handleChange = name => event => {
+    setValues({
+      ...values,
+      error: false,
+      [name]: event.target.value
     });
+  };
 
-    const { email, password, error, loading, redirectToReferrer } = values
-    const { user } = isAuthenticated();
-
-    const handleChange = name => event => {
+  const clickSubmit = event => {
+    event.preventDefault();
+    setValues({
+      ...values,
+      error: false,
+      loading: true
+    });
+    signin({ email, password }).then(data => {
+      if (data.error) {
         setValues({
-            ...values,
-            error: false,
-            [name]: event.target.value
+          ...values,
+          error: data.error,
+          loading: false
         });
-    };
-
-    const clickSubmit = (event) => {
-        event.preventDefault();
-        setValues({
+      } else {
+        authenticate(data, () => {
+          setValues({
             ...values,
-            error: false,
-            loading: true
-        })
-        signin({ email, password })
-            .then(data => {
-                if (data.error) {
-                    setValues({
-                        ...values,
-                        error: data.error,
-                        loading: false
-                    })
-                } else {
-                    authenticate(data, () => {
-                        setValues({
-                            ...values,
-                            redirectToReferrer: true
-                        });
-                    });
-                }
+            redirectToReferrer: true
+          });
+        });
+      }
+    });
+  };
 
-            });
-    };
+  const signUpForm = () => (
+    <form>
+      <div className='form-group'>
+        <label className='text-muted'>Email</label>
+        <input
+          onChange={handleChange('email')}
+          type='email'
+          value={email}
+          className='form-control'
+        />
+      </div>
 
+      <div className='form-group'>
+        <label className='text-muted'>Password</label>
+        <input
+          onChange={handleChange('password')}
+          type='password'
+          value={password}
+          className='form-control'
+        />
+      </div>
 
+      <div>
+        <button onClick={clickSubmit} className='btn btn-primary'>
+          Submit
+        </button>
+      </div>
+    </form>
+  );
 
-    const signUpForm = () => (
-        <form>
-            <div className="form-group">
-                <label className="text-muted">
-                    Email
-                </label>
-                <input
-                    onChange={handleChange('email')}
-                    type="email" value={email}
-                    className="form-control"
-                />
-            </div>
+  const showError = () => (
+    <div
+      className='alert alert-danger'
+      style={{ display: error ? '' : 'none' }}
+    >
+      {error}
+    </div>
+  );
 
-            <div className="form-group">
-                <label className="text-muted">
-                    Password
-                </label>
-                <input
-                    onChange={handleChange('password')}
-                    type="password"
-                    value={password}
-                    className="form-control" />
-            </div>
-
-            <div>
-                <button
-                    onClick={clickSubmit}
-                    className="btn btn-primary"
-                >
-                    Submit
-                </button>
-            </div>
-        </form>
-    )
-
-    const showError = () => (
-        <div
-            className="alert alert-danger"
-            style={{ display: error ? '' : 'none' }}
-        >
-            {error}
-        </div>
-    )
-
-    const showLoading = () => (
-        loading && (
-            <div className="alert alert-info">
-                <h2>Loading...</h2>
-            </div>
-        )
+  const showLoading = () =>
+    loading && (
+      <div className='alert alert-info'>
+        <h2>Loading...</h2>
+      </div>
     );
 
-    const redirectUser = () => {
-        if (redirectToReferrer) {
-            if(user && user.role === 1) {
-                return  <Redirect to='/admin/dashboard' />
-            } else {
-                return <Redirect to="/user/dashboard" />
-            }
-        }
-        if(isAuthenticated()) {
-            return <Redirect to='/'/>
-        }
+  const redirectUser = () => {
+    if (redirectToReferrer) {
+      if (user && user.role === 1) {
+        return <Redirect to='/admin/dashboard' />;
+      } else {
+        return <Redirect to='/user/dashboard' />;
+      }
     }
+    if (isAuthenticated()) {
+      return <Redirect to='/' />;
+    }
+  };
 
-    return (
-        <Layout
-            title="Sign-In Page"
-            description="Sign-In to React E-commerce App"
-            className="container col-md-8 offset-md-2"
-        >
-            {showLoading()}
-            {showError()}
-            {signUpForm()}
-            {redirectUser()}
-            {/* {JSON.stringify(values)} */}
-        </Layout>
-    )
-}
+  return (
+    <Layout
+      title='Sign-In Page'
+      description='Sign-In to React E-commerce App'
+      className='container col-md-8 offset-md-2'
+    >
+      {showLoading()}
+      {showError()}
+      {signUpForm()}
+      {redirectUser()}
+      {/* {JSON.stringify(values)} */}
+    </Layout>
+  );
+};
 
 export default Signin;
